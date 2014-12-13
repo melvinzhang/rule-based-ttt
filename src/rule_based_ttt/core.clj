b36b7aea446dd5bbf642f5aa8cb8aa1af36fe51f
melvinzhang <melvinzhang@gmail.com>
2014-12-13 23:10:54 +0800

    replace test-first/second with method that generates all possible end states

--- src/rule_based_ttt/core.clj
+++ src/rule_based_ttt/core.clj
@@ -1,13 +1,6 @@
 (ns rule-based-ttt.core
   (:gen-class))
 
-; board
-;    0 1 2
-;    -----
-; 0 |2 7 3
-; 1 |6 0 8
-; 2 |1 5 4
-
 (defn other [s]
   (if (= s 'X) 'O 'X))
 
@@ -16,28 +9,6 @@
     (-> board
       (assoc p s)
       (assoc :move (assoc moves p (count moves))))))
-
-(defn basic [s board]
-  (let [pos (for [x (range 0 3) y (range 0 3)] [x y])
-        avail (filter #(nil? (get board %)) pos)
-        t (other s)
-        win (filter #(won s (move board % s)) avail)
-        block (filter #(won t (move board % t)) avail)]
-    (cond
-      (> (count win) 0) (move board (first win) s)
-      (> (count block) 0) (move board (first block) s)
-      :else
-        (condp = nil
-          (get board [1 1]) (move board [1 1] s)
-          (get board [2 0]) (move board [2 0] s)
-          (get board [0 0]) (move board [0 0] s)
-          (get board [0 2]) (move board [0 2] s)
-          (get board [2 2]) (move board [2 2] s)
-          (get board [2 1]) (move board [2 1] s)
-          (get board [1 0]) (move board [1 0] s)
-          (get board [0 1]) (move board [0 1] s)
-          (get board [1 2]) (move board [1 2] s)) 
-      )))
 
 (defn row-won [r s board]
    (= s
@@ -91,50 +62,67 @@
     (print (str (get board [2 1] " ") (get moves [2 1] " ") " "))
     (print (str (get board [2 2] " ") (get moves [2 2] " ") " "))
     (print "\n")
-    (print "\n") 
-    
-    )
-  )
+    (print "\n")))
 
-(defn run [p1 p2 board]
-  (if (done board)
-    (show board)
-    (run p2 p1 (p1 board))))
+; basic AI
+; 1. win if self have two symbols in a row
+; 2. block if opponent have two symbols in a row
+; 3. else put in preference
+; board
+;    0 1 2
+;    -----
+; 0 |2 7 3
+; 1 |6 0 8
+; 2 |1 5 4
+(defn basic [s board]
+  (let [pos (for [x (range 0 3) y (range 0 3)] [x y])
+        avail (filter #(nil? (get board %)) pos)
+        t (other s)
+        win (filter #(won s (move board % s)) avail)
+        block (filter #(won t (move board % t)) avail)]
+    (cond
+      (> (count win) 0) (move board (first win) s)
+      (> (count block) 0) (move board (first block) s)
+      :else
+        (condp = nil
+          (get board [1 1]) (move board [1 1] s)
+          (get board [2 0]) (move board [2 0] s)
+          (get board [0 0]) (move board [0 0] s)
+          (get board [0 2]) (move board [0 2] s)
+          (get board [2 2]) (move board [2 2] s)
+          (get board [2 1]) (move board [2 1] s)
+          (get board [1 0]) (move board [1 0] s)
+          (get board [0 1]) (move board [0 1] s)
+          (get board [1 2]) (move board [1 2] s)) 
+      )))
 
-(defn test-first 
-  ([prog] (test-first #(prog 'X %) {:move {}}))
-  ([prog board]
+; lift a function p from board -> board to board -> [board]
+(defn lift [p s]
+  (fn [board]
+    (if (done board) [board] [(p s board)])))
+
+(defn exhaust [s]
+  (fn [board]
     (if (done board)
-      (if (won 'O board)
-        (do
-          (show board)
-          false)
-        true)
-      (let [nextb (prog board)
-            pos (for [x (range 0 3) y (range 0 3)] [x y])
-            avail (filter #(nil? (get nextb %)) pos)]
-        (if (not (done nextb)) 
-          (for [p avail]
-            (test-first prog (move nextb p 'O)))
-          true)))))
-
-(defn test-second
-  ([prog] (test-second #(prog 'O %) {:move {}}))
-  ([prog board]
-    (if (done board)
-      (if (won 'X board)
-        (do
-          (show board)
-          false)
-        true)
+      [board] 
       (let [pos (for [x (range 0 3) y (range 0 3)] [x y])
             avail (filter #(nil? (get board %)) pos)]
         (for [p avail]
-          (test-second prog (prog (move board p 'X))))))))
+          (move board p s))))))
+
+(defn gen-end-state [p1 p2 boards]
+  (let [res (mapcat p1 boards)]
+    (if (= res boards) boards (gen-end-state p2 p1 (mapcat p1 boards)))))
+
+(defn gen-first [p]
+  (gen-end-state (lift p 'X) (exhaust 'O) [{:move {}}]))
+
+(defn gen-second [p]
+  (gen-end-state (exhaust 'X) (lift p 'O) [{:move {}}]))
 
 (defn -main
   [& args]
   (println "Testing AI as first player")
-  (test-first basic)
+  (map show (filter #(won 'O %) (gen-first basic)))
   (println "Testing AI as second player")
-  (test-second basic))
+  (map show (filter #(won 'X %) (gen-second basic))))
