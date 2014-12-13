eeb9028896db4203699f08369b70dce7bba186f2
melvinzhang <melvinzhang@gmail.com>
2014-12-13 20:24:35 +0800

    include move number in show, add test-second

--- src/rule_based_ttt/core.clj
+++ src/rule_based_ttt/core.clj
@@ -11,26 +11,32 @@
 (defn other [s]
   (if (= s 'X) 'O 'X))
 
-(defn play [s board]
+(defn move [board p s]
+  (let [moves (:move board)]
+    (-> board
+      (assoc p s)
+      (assoc :move (assoc moves p (count moves))))))
+
+(defn basic [s board]
   (let [pos (for [x (range 0 3) y (range 0 3)] [x y])
         avail (filter #(nil? (get board %)) pos)
         t (other s)
-        win (filter #(won s (assoc board % s)) avail)
-        block (filter #(won t (assoc board % t)) avail)]
+        win (filter #(won s (move board % s)) avail)
+        block (filter #(won t (move board % t)) avail)]
     (cond
-      (> (count win) 0) (assoc board (first win) s)
-      (> (count block) 0) (assoc board (first block) s)
+      (> (count win) 0) (move board (first win) s)
+      (> (count block) 0) (move board (first block) s)
       :else
         (condp = nil
-          (get board [1 1]) (assoc board [1 1] s)
-          (get board [2 0]) (assoc board [2 0] s)
-          (get board [0 0]) (assoc board [0 0] s)
-          (get board [0 2]) (assoc board [0 2] s)
-          (get board [2 2]) (assoc board [2 2] s)
-          (get board [2 1]) (assoc board [2 1] s)
-          (get board [1 0]) (assoc board [1 0] s)
-          (get board [0 1]) (assoc board [0 1] s)
-          (get board [1 2]) (assoc board [1 2] s)) 
+          (get board [1 1]) (move board [1 1] s)
+          (get board [2 0]) (move board [2 0] s)
+          (get board [0 0]) (move board [0 0] s)
+          (get board [0 2]) (move board [0 2] s)
+          (get board [2 2]) (move board [2 2] s)
+          (get board [2 1]) (move board [2 1] s)
+          (get board [1 0]) (move board [1 0] s)
+          (get board [0 1]) (move board [0 1] s)
+          (get board [1 2]) (move board [1 2] s)) 
       )))
 
 (defn row-won [r s board]
@@ -71,35 +77,60 @@
       (= (count board) 9)))
 
 (defn show [board]
-  (print (get board [0 0] " "))
-  (print (get board [0 1] " "))
-  (print (get board [0 2] " "))
-  (print "\n")
-  (print (get board [1 0] " "))
-  (print (get board [1 1] " "))
-  (print (get board [1 2] " "))
-  (print "\n")
-  (print (get board [2 0] " "))
-  (print (get board [2 1] " "))
-  (print (get board [2 2] " "))
-  (print "\n")
-  (print "\n"))
+  (let [moves (:move board)]
+    (print "\n")
+    (print (str (get board [0 0] " ") (get moves [0 0] " ") " "))
+    (print (str (get board [0 1] " ") (get moves [0 1] " ") " "))
+    (print (str (get board [0 2] " ") (get moves [0 2] " ") " "))
+    (print "\n")
+    (print (str (get board [1 0] " ") (get moves [1 0] " ") " "))
+    (print (str (get board [1 1] " ") (get moves [1 1] " ") " "))
+    (print (str (get board [1 2] " ") (get moves [1 2] " ") " "))
+    (print "\n")
+    (print (str (get board [2 0] " ") (get moves [2 0] " ") " "))
+    (print (str (get board [2 1] " ") (get moves [2 1] " ") " "))
+    (print (str (get board [2 2] " ") (get moves [2 2] " ") " "))
+    (print "\n")
+    (print "\n") 
+    
+    )
+  )
 
 (defn run [p1 p2 board]
   (if (done board)
     (show board)
     (run p2 p1 (p1 board))))
 
-(defn test-first [prog board depth]
-  (if (done board)
-    (when (won 'O board)
-      (show board))
-    (let [pboard (prog board)
-          pos (for [x (range 0 3) y (range 0 3)] [x y])
-          avail (filter #(nil? (get pboard %)) pos)]
-        (when (not (done board)) 
+(defn test-first 
+  ([prog] (test-first #(prog 'X %) {:move {}}))
+  ([prog board]
+    (if (done board)
+      (if (won 'O board)
+        (do
+          (show board)
+          false)
+        true)
+      (let [nextb (prog board)
+            pos (for [x (range 0 3) y (range 0 3)] [x y])
+            avail (filter #(nil? (get nextb %)) pos)]
+        (if (not (done nextb)) 
           (for [p avail]
-            (test-first prog (assoc pboard p 'O) (+ depth 1)))))))
+            (test-first prog (move nextb p 'O)))
+          true)))))
+
+(defn test-second
+  ([prog] (test-second #(prog 'O %) {:move {}}))
+  ([prog board]
+    (if (done board)
+      (if (won 'X board)
+        (do
+          (show board)
+          false)
+        true)
+      (let [pos (for [x (range 0 3) y (range 0 3)] [x y])
+            avail (filter #(nil? (get board %)) pos)]
+        (for [p avail]
+          (test-second prog (prog (move board p 'X))))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
