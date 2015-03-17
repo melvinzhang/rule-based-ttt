30ef6baac3fe7ce32b8c5ae9843d7c99a378df8f
melvinzhang <melvinzhang@gmail.com>
2015-03-18 07:46:58 +0800

    add stub tests

---
+++ test/rule_based_ttt/core_test.clj
@@ -0,0 +1,7 @@
+(ns rule-based-ttt.core-test
+  (:require [clojure.test :refer :all]
+            [rule-based-ttt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
